a57be4bfa5b2c2b7ac137b44593a10dbab78d2c6
Juliodvp29 <juliodevep29@gmail.com>
2022-05-29 10:01:43 -0500

    login and register

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const app = express();
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/user");
+const authRoutes = require("./routes/auth");
 
 dotenv.config();
 app.use(express.json());
@@ -15,6 +16,7 @@ mongoose
   });
 
 app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("server started at port 3000");
--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const UserSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
-    createdAt: Date.now(),
+    createdAt: { type: Date, default: Date.now },
   },
   { timestamps: true }
 );
